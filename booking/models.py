b50caa40f6330f34e3bd921f8678e2f4a042c744
Miguel Pineda <mixel@Miguels-MacBook-Pro.local>
2024-07-01 17:30:38 -0600

    All backend

---
+++ booking/models.py
@@ -0,0 +1,140 @@
+from django.db import models
+import datetime
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from shortuuid.django_fields import ShortUUIDField
+import uuid
+from django.utils.text import slugify
+from users.models import UserAccount, Profile
+# import store.models as store
+
+
+ICON_TYPE= (
+    ("Bootstrap Icons", "Bootstrap Icons"),
+    ("Fontawesome Icons", "Fontawesome Icons"),
+    ("Box Icons", "Box Icons"),
+    ("Remi Icons", "Remi Icons"),
+    ("Flat Icons", "Flat Icons")
+)
+
+class Package(models.Model):
+    n_people = models.IntegerField(default=30)
+    price = models.FloatField()
+    title = models.CharField(max_length = 255, blank = True)
+    description = models.TextField()
+    hours = models.TextField()
+    icon = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+       ordering = ('n_people',)
+    def __str__(self):
+        return self.title + " de " + str(self.n_people) + " personas"
+
+class Extra(models.Model):
+    title = models.CharField(max_length = 255, blank = True)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class Rule(models.Model):
+    title = models.CharField(max_length = 255)
+    desciption = models.CharField(max_length = 255) 
+    
+    def __str__(self):
+        return self.title
+
+class Venue(models.Model):
+    name = models.CharField(max_length = 255, blank = True)
+    address = models.CharField(max_length = 255, blank = True)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique = True, default="")
+    
+    def __str__(self):
+        return self.name
+    
+    def __save__(self, *args, **kwargs):
+        if self.slug == "" or self.slug is None:
+            self.slug = slugify(self.name)
+        
+        super(Venue, self).save(*args, **kwargs)
+
+
+
+ta = datetime.time(10,00,00)
+td = datetime.time(00,00,00)
+
+class Event(models.Model):
+
+    STATUS = (
+        ("en_carrito", "En carrito"),
+        ("en_revision", "En revisión"),
+        ("aceptado","Aceptado"),
+        ("rechazado", "Rechazado"),
+        ("finalizado", "Finalizado"),
+    )
+    # id = models.AutoField( primary_key=True )
+
+    # pid 
+    #  ShortUUIDField(unique=True, length=20, alphabet='abcdefg12345', default=uuid.uuid4, db_column = "pid")
+    date = models.DateField()
+    # id = models.AutoField( primary_key=True )
+    arrival = models.TimeField(default = ta)
+    departure = models.TimeField(default = td)
+    package = models.ForeignKey(Package, on_delete= models.PROTECT, related_name= "package")
+    extras =  models.ManyToManyField(Extra, null=True,  blank=True)
+    client = models.ForeignKey(UserAccount, on_delete= models.PROTECT, related_name= "client")
+    admin = models.ForeignKey(UserAccount, on_delete= models.PROTECT, related_name= "admin")
+    venue = models.ForeignKey(Venue, on_delete = models.PROTECT, related_name = "venue")
+    created = models.DateTimeField(auto_now_add = True)
+    modified = models.DateTimeField(auto_now = True)
+    advance = models.FloatField(default = 0)
+    status = models.CharField(max_length=100, choices=STATUS, default="en_carrito")
+    rating = models.PositiveIntegerField(default =0,  blank=True)
+    rating_comment = models.CharField(max_length = 255, null = True,  blank=True)
+    slug = models.SlugField(unique = True, default="")
+    payment_intent = models.CharField(max_length=1000, null=True, blank=True)
+    eid = ShortUUIDField(default=uuid.uuid4, unique=True, null=True, length=10, alphabet='abcdefg12345')
+
+    def total_price(self):
+        return self.package.price
+        
+
+    # Calculates the average rating of the product
+    def event_rating(self):
+        from store.models import Review
+        event_rating = Review.objects.filter(event=self).aggregate(avg_rating=models.Avg('rating'))
+        return event_rating['avg_rating']
+    
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.eid + '-' + self.date) 
+        
+        if self.status == 'en_carrito':
+            pass
+                
+        # self.rating = self.event_rating()
+        
+        super(Event, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.client.first_name + " " + self.client.last_name + " el " + str(self.date)   
+
+
+    class Meta:
+        ordering = ['-date']
+
+class VenueFeatures(models.Model):
+    hotel = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    icon_type = models.CharField(max_length=100, null=True, blank=True, choices=ICON_TYPE)
+    icon = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
---
+++ booking/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+from users.views import ProfileView
+
+
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+    path('sitios/', VenueListAPIView.as_view()),
+    path('paquetes/', PackageViewSet.as_view({'get': 'list'})),
+    path('extras/', ExtraListAPIView.as_view()),
+    path('eventos/', EventListAPIView.as_view()),
+    path('add-event/', EventListAPIView.as_view()),
+    path('eventos/ocupados/', DatesOccupiedListAPIVIEW.as_view()),
+    path('eventos/<int:pk>', EventDetailAPIView.as_view()),
+    path('user/profile/<user_id>/', ProfileView.as_view(), name='user_profile'),
+    path('rules/', RuleListAPIView.as_view(), name='rules'),
+    path('sitios-activos', VenueActiveListAPIView.as_view()),
+    path('sitios/<slug>/', VenueDetailAPIView.as_view()),
+
+    #Store endpoints
+    path('cart-view/', CartAddAPIView.as_view()),
+    path('create-order/', CreateOrderAPIView.as_view()),
+
+    path('checkout/<int:pk>', CheckoutAPIView.as_view()),
+    path('coupons/<code>', CouponAPIView.as_view()),
+    path('stripe-checkout/', StripeCheckoutAPI.as_view(), name='stripe-checkout'),
+    # path('payment-success/', PaymentSuccessView.as_view(), name='payment-success'),
+
+]
