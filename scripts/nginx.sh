e4a17d926685540e0313610680022a07807d6c14
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-07-02 13:36:58 -0600

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,7 +5,7 @@ sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/terraza/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/terraza /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/terraza/etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
