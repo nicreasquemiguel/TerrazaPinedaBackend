e2cb702092321bba300cfcaee989309c94ec6c6f
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-07-15 19:37:01 -0600

    gunicorn

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/terraza/nginx/nginx.conf /etc/nginx/sites-available/terraza
-sudo ln -s /etc/nginx/sites-available/terraza /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
+sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
