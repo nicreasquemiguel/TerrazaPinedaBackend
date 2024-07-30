dd1e5dcfc30d0a97dc0d8eb9c179f7cd5d2e306d
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-07-29 23:19:42 -0600

    views

--- booking/urls.py
+++ booking/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('checkout/<int:pk>', CheckoutAPIView.as_view()),
     path('coupons/<code>', CouponAPIView.as_view()),
     path('stripe-checkout/', StripeCheckoutAPI.as_view(), name='stripe-checkout'),
-    path('payment-success/', PaymentSuccessView.as_view(), name='payment-success'),
+    path('payment-success/<order_id>', PaymentSuccessView.as_view(), name='payment-success'),
 
 ]
