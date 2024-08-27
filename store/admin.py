1854bbe1f2758212570edb0be47bb7830659297a
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-08-26 18:04:17 -0600

    views

--- store/admin.py
+++ store/admin.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(CartOrder)
-admin.site.register(Cart)
-admin.site.register(CartOrderItem)
-admin.site.register(Notification)
+
+admin.site.register(PaymentOrder)
 admin.site.register(Review)
 admin.site.register(Coupon)
 admin.site.register(BookingFAQ)
