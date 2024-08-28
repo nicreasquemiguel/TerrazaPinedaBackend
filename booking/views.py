d377bca4638b5075371372e59d1421efbf785d8c
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-08-27 23:00:41 -0600

    views

--- booking/views.py
+++ booking/views.py
@@ -312,6 +312,7 @@ class OrdersView(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         print(self.request.user.id)
+        print(self.request.data)
         return PaymentOrder.objects.all().filter(payer = self.request.user.id)
 
 
