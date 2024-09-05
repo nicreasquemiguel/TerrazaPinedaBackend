61cdd034b72cf03c4a4cc885b0456943bb3eca52
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-09-04 19:14:17 -0600

    views

--- terraza/settings.py
+++ terraza/settings.py
@@ -163,7 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'terrazapineda7@gmail.com'
-EMAIL_HOST_PASSWORD = 'pptqitvhjgjvhoxt'
+EMAIL_HOST_PASSWORD = 'Terraza-123'
 EMAIL_USE_TLS = True
 
 
