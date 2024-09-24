6c1a1d7254817f915fb9ee358c0fd14402a3560d
nicreasquemiguel <64338585+nicreasquemiguel@users.noreply.github.com>
2024-09-24 16:53:12 -0600

    views

--- booking/urls.py
+++ booking/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('extras/', ExtraListAPIView.as_view()),
     path('eventos/', EventListAPIView.as_view()),
     path('add-event/', EventCreateApiView.as_view()),
-    path('approve/<eid>/', EventApproveAPIView.as_view())
+    path('approve/<eid>/', EventApproveAPIView.as_view()),
     path('eventos/ocupados/', DatesOccupiedListAPIVIEW.as_view()),
     path('eventos/<eid>', MyEventAPIView.as_view()),
     path('user/profile/<user_id>/', ProfileView.as_view(), name='user_profile'),
